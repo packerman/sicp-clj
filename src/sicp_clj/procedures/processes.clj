7429117d8f7c9f9c8073f5eebef20ed59913ffae
Piotr Ackermann <pioacker@gmail.com>
2016-07-09 02:05:02 +0200

    sine

--- src/sicp_clj/procedures/processes.clj
+++ src/sicp_clj/procedures/processes.clj
@@ -1,23 +1,12 @@
-(ns sicp-clj.procedures.processes)
+(ns sicp-clj.procedures.processes
+  (:require [sicp-clj.procedures.elements :refer [cube abs]]))
 
 (defn factorial-rec [n]
   (if (= n 1)
     1
-    (* n (factorial-rec (dec n)))))
-
-(defn factorial-rec' [n]
-  (if (= n 1)
-    1
-    (*' n (factorial-rec' (dec n)))))
+    (*' n (factorial-rec (dec n)))))
 
 (defn factorial [n]
-  (letfn [(fact-iter [product counter]
-            (if (> counter n)
-              product
-              (recur (* counter product) (inc counter))))]
-    (fact-iter 1 1)))
-
-(defn factorial' [n]
   (letfn [(fact-iter [product counter]
             (if (> counter n)
               product
@@ -41,13 +30,6 @@
   (letfn [(fib-iter [a b count]
             (if (= count 0)
               b
-              (recur (+ a b) a (dec count))))]
-    (fib-iter 1 0 n)))
-
-(defn fib' [n]
-  (letfn [(fib-iter [a b count]
-            (if (= count 0)
-              b
               (recur (+' a b) a (dec count))))]
     (fib-iter 1 0 n)))
 
@@ -67,3 +49,23 @@
               4 25
               5 50))]
     (cc amount 5)))
+
+(defn f-rec [n]
+  (if (< n 3)
+    n
+    (+' (f-rec (- n 1)) (*' 2 (f-rec (- n 2))) (*' 3 (f-rec (- n 3))))))
+
+(defn f [n]
+  (letfn [(f-iter [a b c i]
+            (if (zero? i)
+              c
+              (recur (+' a (*' 2 b) (*' 3 c)) a b (dec i))))]
+    (f-iter 2 1 0 n)))
+
+(defn sine [angle]
+  (let [p #(- (* 3 %) (* 4 (cube %)))
+        double-pi (* 2 Math/PI)]
+    (cond
+      (>= (abs angle) double-pi) (sine (rem angle double-pi))
+      (>= (abs angle) 0.1) (p (sine (/ angle 3.0)))
+      :else angle)))
