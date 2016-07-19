1c8baaab468470a10887e60890a52de5f0e3112d
Piotr Ackermann <pioacker@gmail.com>
2016-07-19 23:56:50 +0200

    tree structures

--- src/sicp_clj/data/abstraction.clj
+++ src/sicp_clj/data/abstraction.clj
@@ -48,7 +48,7 @@
 
 (defn make-rectangle [left-bottom-point right-top-point]
   {:left-bottom left-bottom-point
-   :right-top right-top-point})
+   :right-top   right-top-point})
 
 (defn width-rectangle [{{left-bottom-x :x} :left-bottom {right-top-x :x} :right-top}]
   (abs (- right-top-x left-bottom-x)))
@@ -90,8 +90,52 @@
 
 (defn my-reverse [lst]
   (letfn [(iter [rev lst]
-            (if (empty? lst)
-              rev
-              (let [[x & xs] lst]
-                (recur (cons x rev) xs))))]
+            (if-let [[x & xs] lst]
+              (recur (cons x rev) xs)
+              rev))]
     (iter '() lst)))
+
+(defn cc [amount coins]
+  (cond
+    (zero? amount) 1
+    (or (neg? amount) (empty? coins)) 0
+    :else (+ (cc amount (rest coins))
+             (cc (- amount (first coins)) coins))))
+
+(defn same-parity [& lst]
+  (when-let [[x & xs] lst]
+    (cons x
+          (filter #(even? (- % x)) xs))))
+
+(defn count-leaves [x]
+  (if (seq? x)
+    (reduce + (map count-leaves x))
+    1))
+
+(defn deep-reverse [lst]
+  (if (not (seq? lst))
+    lst
+    (reverse (map deep-reverse lst))))
+
+(defn fringe [x]
+  (if (not (seq? x))
+    (list x)
+    (mapcat fringe x)))
+
+(defn make-mobile [left right]
+  (list left right))
+
+(defn make-branch [length structure]
+  (list length structure))
+
+(defn total-weight [mobile]
+  (letfn [(branch-weight [[length structure]]
+            (if (number? structure)
+              structure
+              (mobile-weight structure)))
+          (mobile-weight [[left right]]
+            (+ (branch-weight left)
+               (branch-weight right)))]
+    (mobile-weight mobile)))
+
+;TODO Exercise 2.29 3. (balanced)
