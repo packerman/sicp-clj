1b0342a3d2f05c23d383a17f5d8507ca2ab89195
Piotr Ackermann <pioacker@gmail.com>
2016-08-05 23:02:56 +0200

    complex numbers - roots and powers

--- src/sicp_clj/data/complex_numbers.clj
+++ src/sicp_clj/data/complex_numbers.clj
@@ -54,6 +54,39 @@
     (- (angle z1) (angle z2))))
 
 
+(defn nth-roots [z n]
+  (let [r (magnitude z)
+        a (angle z)
+        r-nth-root (Math/pow r (/ 1.0 n))]
+    (for [k (range n)]
+      (->Polar r-nth-root
+               (/ (+ a (* 2 k Math/PI)) n)))))
+
+(defn nth-power
+  ([z n]
+    (nth-power z n (->Rectangular 1 0)))
+  ([z n acc]
+    (if (zero? n)
+      acc
+      (recur z (dec n) (mul-complex acc z)))))
+
+
+(defmulti str-complex class)
+
+(defmethod str-complex Rectangular [{:keys [real imaginary]}]
+  (str "rect(" real ", " imaginary ")"))
+
+(defmethod str-complex Polar [{:keys [magnitude angle]}]
+  (str "polar(" magnitude ", " angle ")"))
+
+(defmulti rectangular class)
+
+(defmethod rectangular Rectangular [z] z)
+
+(defmethod rectangular Polar [z]
+  (->Rectangular (real-part z) (imag-part z)))
+
+
 (defn equal-complex? [z1 z2]
   (zero? (magnitude (sub-complex z1 z2))))
 
@@ -66,7 +99,7 @@
         i (->Rectangular 0 1)
         real (fn [x] (->Rectangular x 0))]
     (is (almost-equal? tolerance
-                       (->Rectangular -1 0) (mul-complex i i)))
+                       (real -1) (mul-complex i i)))
     (is (almost-equal? tolerance
                        (->Rectangular 5 3) (add-complex (real 5) (mul-complex (real 3) i))))
     (is (almost-equal? tolerance
