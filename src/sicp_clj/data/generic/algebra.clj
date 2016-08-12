1d7380f55816b7722b0476eb99eff7a357cd4724
Piotr Ackermann <pioacker@gmail.com>
2016-08-12 23:18:54 +0200

    polynomial multiplication

--- src/sicp_clj/data/generic/algebra.clj
+++ src/sicp_clj/data/generic/algebra.clj
@@ -12,11 +12,14 @@
 (defn poly? [p]
   (= ::Polynomial (type p)))
 
+(defn zero-poly? [{:keys [terms] :as p}]
+  {:pre [(poly? p)]}
+  (empty? terms))
+
 (defn make-term [order coeff]
   [order coeff])
 
 (declare add-terms
-         mul-terms
          adjoin-term)
 
 (defn add-poly [{variable-1 :variable terms-1 :terms :as p1}
@@ -25,15 +28,13 @@
   (make-poly variable-1
              (add-terms terms-1 terms-2)))
 
+(declare mul-terms)
+
 (defn mul-poly [{variable-1 :variable terms-1 :terms :as p1}
                 {variable-2 :variable terms-2 :terms :as p2}]
   {:pre [(poly? p1) (poly? p2) (= variable-1 variable-2)]}
   (make-poly variable-1
              (mul-terms terms-1 terms-2)))
-
-(defn zero-poly? [{:keys [terms] :as p}]
-  {:pre [(poly? p)]}
-  (empty? terms))
 
 (defn- add-terms [terms-1 terms-2]
   (let [[[order-1 coeff-1 :as t1] & rest-terms-1 :as seq-1] (seq terms-1)
@@ -42,11 +43,19 @@
       (not seq-1) seq-2
       (not seq-2) seq-1
       (> order-1 order-2) (adjoin-term t1 (add-terms rest-terms-1 seq-2))
-      (< order-1 order-2) (adjoin-term t2 (add-terms seq-1 rest-terms-1))
+      (< order-1 order-2) (adjoin-term t2 (add-terms seq-1 rest-terms-2))
       :else (adjoin-term (make-term order-1 (add coeff-1 coeff-2))
                          (add-terms rest-terms-1 rest-terms-2)))))
 
-(defn- mul-terms [terms-1 terms-2])
+(defn- mul-terms [terms-1 terms-2]
+  (letfn [(mul-term-by-all-terms [[order-1 coeff-1 :as t] terms]
+            (when-let [[[order-2 coeff-2 :as first-term] & rest-terms] (seq terms)]
+              (adjoin-term (make-term (+ order-1 order-2)
+                                      (mul coeff-1 coeff-2))
+                           (mul-term-by-all-terms t rest-terms))))]
+    (when-let [[t1 & rest-terms-1] (seq terms-1)]
+      (add-terms (mul-term-by-all-terms t1 terms-2)
+                 (mul-terms rest-terms-1 terms-2)))))
 
 (defn- adjoin-term [[_ coeff :as t] terms]
   (if (=zero? coeff)
@@ -67,7 +76,10 @@
               (make-poly 'x '([3 1] [1 3] [0 -6]))
               (make-poly 'x '([2 -2] [1 1] [0 -2]))))))
 
-(deftest multiplying)
+(deftest multiplying
+  (is (= (make-poly 'x '([6 2] [5 4] [4 1] [3 11] [2 2] [1 4] [0 4]))
+         (mul (make-poly 'x '([3 1] [2 2] [0 4]))
+              (make-poly 'x '([3 2] [1 1] [0 1]))))))
 
 (deftest polynomials
   (adding)
