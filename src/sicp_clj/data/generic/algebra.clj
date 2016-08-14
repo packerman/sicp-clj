d5e96fe23e8bbcc8ab0950ee46cfaf8e76b37da1
Piotr Ackermann <pioacker@gmail.com>
2016-08-14 21:41:29 +0200

    polynomial division

--- src/sicp_clj/data/generic/algebra.clj
+++ src/sicp_clj/data/generic/algebra.clj
@@ -28,13 +28,19 @@
   (make-poly variable-1
              (add-terms terms-1 terms-2)))
 
+(declare neg-terms)
+
 (defn neg-poly [{:keys [variable terms] :as p}]
   {:pre [(poly? p)]}
-  (make-poly 'x (for [[order coeff] terms] [order (neg coeff)])))
+  (make-poly variable (neg-terms terms)))
 
-(defn sub-poly [p1 p2]
-  {:pre [(poly? p1) (poly? p2)]}
-  (add-poly p1 (neg-poly p2)))
+(declare sub-terms)
+
+(defn sub-poly [{variable-1 :variable terms-1 :terms :as p1}
+                {variable-2 :variable terms-2 :terms :as p2}]
+  {:pre [(poly? p1) (poly? p2) (= variable-1 variable-2)]}
+  (make-poly variable-1
+             (sub-terms terms-1 terms-2)))
 
 (declare mul-terms)
 
@@ -43,6 +49,19 @@
   {:pre [(poly? p1) (poly? p2) (= variable-1 variable-2)]}
   (make-poly variable-1
              (mul-terms terms-1 terms-2)))
+
+(declare div-terms)
+
+(defn make-div-result [quot rem]
+  {:quot quot
+   :rem rem})
+
+(defn div-poly [{variable-1 :variable terms-1 :terms :as p1}
+                {variable-2 :variable terms-2 :terms :as p2}]
+  {:pre [(poly? p1) (poly? p2) (= variable-1 variable-2)]}
+  (let [{:keys [quot rem]} (div-terms terms-1 terms-2)]
+    (make-div-result (make-poly variable-1 quot)
+                     (make-poly variable-1 rem))))
 
 (defn- add-terms [terms-1 terms-2]
   (let [[[order-1 coeff-1 :as t1] & rest-terms-1 :as seq-1] (seq terms-1)
@@ -55,15 +74,35 @@
       :else (adjoin-term (make-term order-1 (add coeff-1 coeff-2))
                          (add-terms rest-terms-1 rest-terms-2)))))
 
-(defn- mul-terms [terms-1 terms-2]
+(defn- neg-terms [terms]
+  (for [[order coeff] terms] [order (neg coeff)]))
+
+(defn- sub-terms [terms-1 terms-2]
+  (add-terms terms-1 (neg-terms terms-2)))
+
+(defn mull-term-by-all-terms [t terms]
   (letfn [(mul-2-terms [[order-1 coeff-1] [order-2 coeff-2]]
             (make-term (+ order-1 order-2)
-                       (mul coeff-1 coeff-2)))
-          (mull-term-by-all-terms [t terms]
-            (map #(mul-2-terms t %) terms))]
-    (reduce add-terms
-            nil
-            (map #(mull-term-by-all-terms % terms-2) terms-1))))
+                       (mul coeff-1 coeff-2)))]
+    (map #(mul-2-terms t %) terms)))
+
+(defn- mul-terms [terms-1 terms-2]
+  (reduce add-terms
+          nil
+          (map #(mull-term-by-all-terms % terms-2) terms-1)))
+
+(defn- div-terms [terms-1 terms-2]
+  (if-let [[[order-1 coeff-1] & rest-terms-1] (seq terms-1)]
+    (let [[[order-2 coeff-2] & rest-terms-2] terms-2]
+      (if (> order-2 order-1)
+        (make-div-result '() terms-1)
+        (let [new-term (make-term (- order-1 order-2)
+                                  (div coeff-1 coeff-2))
+              rest-result (div-terms (sub-terms terms-1
+                                               (mull-term-by-all-terms new-term terms-2))
+                                     terms-2)]
+          (make-div-result (adjoin-term new-term (:quot rest-result)) (:rem rest-result)))))
+    (make-div-result '() '())))
 
 (defn- adjoin-term [[_ coeff :as t] terms]
   (if (=zero? coeff)
@@ -72,9 +111,9 @@
 
 (install-type ::Polynomial
               :add add-poly
-              :sub (fn [a b] (add-poly a (neg-poly b)))
+              :sub sub-poly
               :mul mul-poly
-              :div (fn [_ _])
+              :div div-poly
               :zero zero-poly?
               :neg neg-poly
               :raise identity)
@@ -99,11 +138,22 @@
          (mul (make-poly 'x '([3 1] [2 2] [0 4]))
               (make-poly 'x '([3 2] [1 1] [0 1]))))))
 
+(deftest dividing
+  (let [dividend (make-poly 'x '([5 1] [0 -1]))
+        divisor (make-poly 'x '([2 1] [0 -1]))
+        quotient (make-poly 'x '([3 1] [1 1]))
+        remainder (make-poly 'x '([1 1] [0 -1]))]
+    (let [{:keys [quot rem]} (div dividend divisor)]
+      (is (= quotient quot))
+      (is (= remainder rem))
+      (is (= dividend (add (mul quot divisor) rem))))))
+
 (deftest polynomials
   (adding)
   (negating)
   (subtracting)
-  (multiplying))
+  (multiplying)
+  (dividing))
 
 (defn test-ns-hook []
   (polynomials))
