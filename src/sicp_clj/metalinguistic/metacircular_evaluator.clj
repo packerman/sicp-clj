533c65ecb24430f1a9387501f66bc8301ba1052d
Piotr Ackermann <pioacker@gmail.com>
2016-11-01 11:23:50 +0100

    metacircular evaluator
    - lambda
    -sequence

--- src/sicp_clj/metalinguistic/metacircular_evaluator.clj
+++ src/sicp_clj/metalinguistic/metacircular_evaluator.clj
@@ -3,9 +3,9 @@
   (:require [clojure.test :refer :all]
             [sicp-clj.core :refer :all]))
 
-(defn exp-with-env [exp env]
-  {:expression exp
-   :environment env})
+(defrecord Evaluated [expression environment])
+
+(defrecord Procedure [parameters body env])
 
 (defn eval [exp env]
   (letfn [(self-evaluating? [exp]
@@ -27,47 +27,80 @@
             (let [[_ var new-val-expr] exp]
               (if (contains? env var)
                 (let [{new-val :expression new-env :environment} (eval new-val-expr env)]
-                  (exp-with-env nil (assoc new-env var new-val)))
+                  (->Evaluated nil (assoc new-env var new-val)))
                 (error "Unbound variable: SET! " var))))
           (definition? [exp]
             (tagged-list? exp 'define))
           (eval-definition [exp env]
             (let [[_ var new-val-expr] exp
                   {new-val :expression new-env :environment} (eval new-val-expr env)]
-              (exp-with-env nil (assoc new-env var new-val))))
+              (->Evaluated nil (assoc new-env var new-val))))
           (if? [exp]
             (tagged-list? exp 'if))
           (boolean? [exp] (contains? #{'true 'false} exp))
           (true? [exp]
-            (= 'true exp))
+            (not (false? exp)))
+          (false? [exp]
+            (or (= 'false exp) (= nil exp) (= '() exp)))
           (eval-if [exp env]
             (let [[_ condition if-branch else-branch] exp
                   {cond-val :expression new-env :environment} (eval condition env)]
               (if (true? cond-val)
                 (eval if-branch new-env)
-                (eval else-branch new-env))
-              )
-            )
+                (eval else-branch new-env))))
+          (lambda? [exp]
+            (tagged-list? exp 'lambda))
+          (eval-lambda [exp env]
+            (let [[_ parameters body] exp]
+              (->Evaluated (->Procedure parameters body env)
+                           env)))
+          (begin? [exp]
+            (tagged-list? exp 'begin))
+          (eval-sequence [exps env]
+            (if (not (next exps))
+              (eval (first exps) env)
+              (let [{new-env :environment} (eval (first exps) env)]
+                (recur (rest exps)
+                       new-env))))
           ]
     (cond
-      (self-evaluating? exp) (exp-with-env exp env)
-      (variable? exp) (exp-with-env (lookup-variable-value exp env) env)
-      (quoted? exp) (exp-with-env (text-of-quotation exp) env)
+      (self-evaluating? exp) (->Evaluated exp env)
+      (variable? exp) (->Evaluated (lookup-variable-value exp env) env)
+      (quoted? exp) (->Evaluated (text-of-quotation exp) env)
       (assignment? exp) (eval-assignment exp env)
       (definition? exp) (eval-definition exp env)
       (if? exp) (eval-if exp env)
+      (lambda? exp) (eval-lambda exp env)
+      (begin? exp) (eval-sequence (rest exp) env)
       :else (error "Unknown expression type: EVAL " exp))))
+
+(defmacro expression-is [expected tested]
+  `(is (= ~expected (:expression ~tested))))
 
 (deftest evaluator-test
   (testing "Self-evaluating expressions"
-    (is (= (exp-with-env 5 {}) (eval '5 {})))
-    (is (= (exp-with-env 3 {'x 3}) (eval 'x {'x 3}))))
+    (is (= (->Evaluated 5 {}) (eval '5 {})))
+    (is (= (->Evaluated 3 {'x 3}) (eval 'x {'x 3}))))
   (testing "Quoting"
-    (is (= (exp-with-env 'x {}) (eval ''x {}))))
+    (is (= (->Evaluated 'x {}) (eval ''x {}))))
   (testing "Assignment and definition"
     (is (= {'x 5} (:environment (eval '(set! x 5) {'x 3}))))
     (is (= {'x 5} (:environment (eval '(define x 5) {'x 3}))))
     (is (= {'x 5} (:environment (eval '(define x 5) {})))))
   (testing "If expression"
     (is (= 2 (:expression (eval '(if true 2 3) {}))))
-    (is (= 3 (:expression (eval '(if false 2 3) {}))))))
+    (is (= 3 (:expression (eval '(if false 2 3) {})))))
+  (testing "Lambda"
+    (is (= (->Procedure '[x y]
+                        '(if x x y)
+                        '{x 3})
+           (:expression
+             (eval '(lambda [x y] (if x x y))
+                   '{x 3})))))
+  (testing "Sequence"
+    (is (= (->Evaluated 5 '{x 5})
+           (eval '(begin
+                    (define x 3)
+                    (set! x 5)
+                    x)
+                 {})))))
